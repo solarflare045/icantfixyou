cae0b990b486257f4505daff8de96b21db85d131
Max Sorensen <max.sorensen@myriad-it.com>
2017-02-10 12:17:48 +1100

    Location's damage report now contains a loading spinner.

--- src/pages/terminal/location/location.ts
+++ src/pages/terminal/location/location.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
+import { Ailment } from '../../../models/ailment.model';
 import { Location } from '../../../models/object.model';
 
 @Component({
   templateUrl: 'location.html',
   selector: 'location',
 })
-export class LocationComponent {
+export class LocationComponent implements AfterViewInit {
   @Input() location: Location;
+
+  ailments$: Observable<Ailment[]>;
+
+  ngAfterViewInit() {
+    this.ailments$ = this.location.ailments$.startWith(null).share();
+  }
 }
